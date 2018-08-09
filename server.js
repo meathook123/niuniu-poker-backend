5df117f1f3b6bcc21edf36ef5f57fa3cf1d33493
Ruiyang Sun <sunruiyang09@gmail.com>
2018-08-09 16:49:51 -0700

    test

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ function respond(req, res, next) {
   next();
 }
 
+function test(req, res, next) {
+  res.send('test');
+  next();
+}
+
 var server = restify.createServer({
   name: 'niuniu-poker-backend',
   url: 'localhost'
 });
 server.get('/', respond);
+server.get('/test', test);
 
-server.listen(8080, function() {
+server.listen(8080, '127.0.0.1', function() {
   console.log('%s listening at %s', server.name, server.url);
 });
